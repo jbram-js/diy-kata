04142d1e6fff939b476fc6403a68c8b508e82c40
“Joe <josephbramwell@bolstersystems.com>
2020-05-07 20:08:26 +0100

    human cat dog years solved

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,9 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+  const catYears = 15 + 9 + (number - 2) * 4;
+  const dogYears = 15 + 9 + (number - 2) * 5;
+  const allAges = [number, catYears, dogYears];
+
+  return allAges;
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,10 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("return an array of 3 numbers when passed a number reperesenting a humans age. First number will be age in human years, second age in cat years, third age in dog years", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(15)).toEqual([15, 76, 89]);
+    expect(humanCatDogYears(7.5)).toEqual([7.5, 46, 51.5]);
+    expect(humanCatDogYears(10538)).toEqual([10538, 42168, 52704]);
+  });
+});
