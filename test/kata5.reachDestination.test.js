2a7083cdd5b1d4e1103d2f85c88e881f34919fbe
“Joe <josephbramwell@bolstersystems.com>
2020-05-07 20:36:15 +0100

    reach destination solved

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(12345, 100)).toBe("I should be there in 123.5 hours.");
+    expect(reachDestination(123.33, 10.5)).toBe("I should be there in 11.5 hours.");
+  });
 });
